eb829ac8edd2f8d40b6019f9fa5ea5d249f3f7c8
Henry Oberholtzer <henryoberholtzer@gmail.com>
2023-11-07 10:03:30 -0800

    pass test 4
    Co-authored-by: Kim Robinson <kimmykokonut@gmail.com>
    
    Co-authored-by: Ravin Fisher <ravesterrr@gmail.com>

--- __tests__/sudoku.test.js
+++ __tests__/sudoku.test.js
@@ -16,6 +16,29 @@ describe('SudokuPuzzle', () => {
         const newSudoku = new SudokuPuzzle(
             1,2,3,4,5,6,7,8,9,
             1,2,3,4,5,6,7,8,9);
+        expect(newSudoku.rows.row1).toEqual([1,2,3,4,5,6,7,8,9]);
         expect(newSudoku.rows.row2).toEqual([1,2,3,4,5,6,7,8,9]);
+    });
+
+    test('should return an object with 9 rows and 9 arrays with nine numbers in each', () => {
+        const newSudoku = new SudokuPuzzle(
+            1,5,2,4,8,9,3,7,6,
+            7,3,9,2,5,6,8,4,1,
+            4,6,8,3,7,1,2,9,5,
+            3,8,7,1,2,4,6,5,9,
+            5,9,1,7,6,3,4,2,8,
+            2,4,6,8,9,5,7,1,3,
+            9,1,4,6,3,7,5,8,2,
+            6,2,5,9,4,8,1,3,7,
+            8,7,3,5,1,2,9,6,4);
+        expect(newSudoku.rows.row1).toEqual([1,5,2,4,8,9,3,7,6]);
+        expect(newSudoku.rows.row2).toEqual([7,3,9,2,5,6,8,4,1]);
+        expect(newSudoku.rows.row3).toEqual([4,6,8,3,7,1,2,9,5]);
+        expect(newSudoku.rows.row4).toEqual([3,8,7,1,2,4,6,5,9]);
+        expect(newSudoku.rows.row5).toEqual([5,9,1,7,6,3,4,2,8]);
+        expect(newSudoku.rows.row6).toEqual([2,4,6,8,9,5,7,1,3]);
+        expect(newSudoku.rows.row7).toEqual([9,1,4,6,3,7,5,8,2]);
+        expect(newSudoku.rows.row8).toEqual([6,2,5,9,4,8,1,3,7]);
+        expect(newSudoku.rows.row9).toEqual([8,7,3,5,1,2,9,6,4]);
     });
 });
