b300b20545bcfb468e1b38fc70d9f06bac4cb721
Henry Oberholtzer <henryoberholtzer@gmail.com>
2023-11-07 09:38:41 -0800

    pass test 2
    Co-authored-by: Kim Robinson <kimmykokonut@gmail.com>
    
    Co-authored-by: Ravin Fisher <ravesterrr@gmail.com>

--- __tests__/sudoku.test.js
+++ __tests__/sudoku.test.js
@@ -6,4 +6,9 @@ describe('SudokuPuzzle', () => {
         const row1 = new SudokuPuzzle(1,2,3,4,5,6,7,8,9);
         expect(row1.row1[0]).toEqual(1);
     });
+
+    test('should return object with 9 numbers in set array', () => {
+        const newSudoku = new SudokuPuzzle(1,2,3,4,5,6,7,8,9);
+        expect(newSudoku.row1).toEqual([1,2,3,4,5,6,7,8,9]);
+    });
 });
--- src/sudoku.js
+++ src/sudoku.js
@@ -1,5 +1,5 @@
 export default SudokuPuzzle;
 
-function SudokuPuzzle(a1) {
-    this.row1 = [a1];
+function SudokuPuzzle(a1, a2, a3, a4, a5, a6, a7, a8, a9) {
+    this.row1 = [a1, a2, a3, a4, a5, a6, a7, a8, a9];
 }
