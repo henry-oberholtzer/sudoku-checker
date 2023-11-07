30b76d1cd51f9289cf9ac30b513e2c221bdf5453
Henry Oberholtzer <henryoberholtzer@gmail.com>
2023-11-07 09:43:47 -0800

    refactor test one and two
    Co-authored-by: Kim Robinson <kimmykokonut@gmail.com>
    
    Co-authored-by: Ravin Fisher <ravesterrr@gmail.com>

--- __tests__/sudoku.test.js
+++ __tests__/sudoku.test.js
@@ -4,11 +4,11 @@ describe('SudokuPuzzle', () => {
 
     test('should return object with numbers in set array', () => {
         const row1 = new SudokuPuzzle(1,2,3,4,5,6,7,8,9);
-        expect(row1.row1[0]).toEqual(1);
+        expect(row1.rows.row1[0]).toEqual(1);
     });
 
     test('should return object with 9 numbers in set array', () => {
         const newSudoku = new SudokuPuzzle(1,2,3,4,5,6,7,8,9);
-        expect(newSudoku.row1).toEqual([1,2,3,4,5,6,7,8,9]);
+        expect(newSudoku.rows.row1).toEqual([1,2,3,4,5,6,7,8,9]);
     });
 });
--- src/sudoku.js
+++ src/sudoku.js
@@ -1,5 +1,10 @@
 export default SudokuPuzzle;
 
 function SudokuPuzzle(a1, a2, a3, a4, a5, a6, a7, a8, a9) {
-    this.row1 = [a1, a2, a3, a4, a5, a6, a7, a8, a9];
+    this.rows = {
+        row1: [a1, a2, a3, a4, a5, a6, a7, a8, a9],
+        row2: [0]
+    };
+    
+    
 }
